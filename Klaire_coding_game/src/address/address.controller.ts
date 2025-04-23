3042f2778041add51cf440eeff34d662592ade20
SkanderChayoukhi <skanderchayoukhi995@gmail.com>
2025-04-23 18:46:17 +0200

    updated error handling in the address controller

--- Klaire_coding_game/src/address/address.controller.ts
+++ Klaire_coding_game/src/address/address.controller.ts
@@ -1,36 +1,30 @@
-import { Controller, Post, Body, Get, Param, HttpException, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, NotFoundException } from '@nestjs/common';
 import { AddressService } from './address.service';
 import { CreateAddressDto } from './dto/create-address.dto';
 
-
 @Controller('api/addresses')
 export class AddressController {
-    constructor(private readonly addressService: AddressService) { }
-    @Post()
-    async create(@Body() createAddressDto: CreateAddressDto) {
-        try {
-            const address = await this.addressService.create(createAddressDto.q);
-            if (!address) {
-                throw new HttpException('Adresse non trouvée. Aucun résultat ne correspond à votre recherche.', HttpStatus.NOT_FOUND);
-            }
-            return address;
-        } catch (error) {
-            if (error instanceof HttpException) throw error; // Rethrow known HTTP exceptions
-            throw new HttpException("Erreur serveur : impossible de contacter l'API externe.", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+  constructor(private readonly addressService: AddressService) {}
 
-    @Get(':id/risks')
-    async getRisks(@Param('id') id: string) {
-        try {
-            const risks = await this.addressService.getRisks(+id);
-            if (!risks) {
-                throw new HttpException('Adresse non trouvée.', HttpStatus.NOT_FOUND);
-            }
-            return risks;
-        } catch (error) {
-            if (error instanceof HttpException) throw error; // Rethrow known HTTP exceptions
-            throw new HttpException("Erreur serveur : échec de la récupération des données de Géorisques.", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+  @Post()
+  async create(@Body() createAddressDto: CreateAddressDto) {
+    const address = await this.addressService.create(createAddressDto.q);
+    if (!address) {
+      throw new NotFoundException({
+        error: "Adresse non trouvée. Aucun résultat ne correspond à votre recherche.",
+      });
     }
+    return address;
+  }
+
+  @Get(':id/risks')
+  async getRisks(@Param('id') id: string) {
+    const risks = await this.addressService.getRisks(+id);
+    if (!risks) {
+      throw new NotFoundException({
+        error: "Adresse non trouvée.",
+      });
+    }
+    return risks;
+  }
 }
