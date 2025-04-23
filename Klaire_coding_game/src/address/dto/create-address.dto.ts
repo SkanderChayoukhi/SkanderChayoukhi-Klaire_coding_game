a57a2c024925f66bbc6491839a929df0ff7d358b
SkanderChayoukhi <skanderchayoukhi995@gmail.com>
2025-04-23 18:49:53 +0200

    added error messages in DTO validation

--- Klaire_coding_game/src/address/dto/create-address.dto.ts
+++ Klaire_coding_game/src/address/dto/create-address.dto.ts
@@ -1,7 +1,7 @@
-import { IsString, IsNotEmpty, MinLength } from 'class-validator';
+import { IsString, IsNotEmpty, MinLength } from "class-validator";
 
 export class CreateAddressDto {
-  @IsString({ message: "Le champ 'q' doit être une chaîne de caractères." })
+  @IsString({message: "Le champ 'q' doit être une chaîne de caractères."})
   @IsNotEmpty({ message: "Le champ 'q' est requis." })
   @MinLength(1, { message: "Le champ 'q' doit être une chaîne non vide." })
   q: string;
