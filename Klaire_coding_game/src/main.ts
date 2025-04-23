f83fba2ed2a6e1db6ace4f2381e96d4eabe7939c
SkanderChayoukhi <skanderchayoukhi995@gmail.com>
2025-04-23 18:36:06 +0200

    added global validation pipe and global exception filter in main.ts

--- Klaire_coding_game/src/main.ts
+++ Klaire_coding_game/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, BadRequestException } from '@nestjs/common';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors) => {
+        const firstError = errors[0];
+        const errorMessage = Object.values(firstError.constraints)[0];
+        return new BadRequestException({
+          error: errorMessage,
+        });
+      },
+    }),
+  );
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  await app.listen(8000);
 }
 bootstrap();
